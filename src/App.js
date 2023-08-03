028b1027b3d8a152976d5060221e890945be301f
Neema <neemanachok29@gmail.com>
2023-08-03 22:49:21 +0300

    Your commit message here

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WeatherApp from './components/WeatherApp';
+
+function App(){
+  return (
+    <div className="bg-slate-100 min-h-screen">
+      <WeatherApp />
+    </div>
+  );
+};
+
+export default App;
